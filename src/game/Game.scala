03e463124da89fdfa9f356cd6e352beb43571764
Gabriel Such <gabrielsuch@gmail.com>
2012-01-18 00:49:39 -0200

    Remove unnecessary imports

--- src/game/Game.scala
+++ src/game/Game.scala
@@ -1,24 +1,13 @@
 package game
 
-import java.awt.event.ActionListener
-import scala.swing.event.ActionEvent
 import adapter.SwingAdapter
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JLabel
 import scala.swing.Button
-import scala.swing.SimpleGUIApplication
 import scala.swing.MainFrame
-import scala.swing.event.EditDone
-import scala.swing.FlowPanel
-import scala.swing.TextField
-import scala.swing.Label
 import scala.swing.Swing
 import scala.swing.BoxPanel
 import scala.swing.event.ButtonClicked
 import scala.swing.Orientation
 import scala.swing.SimpleSwingApplication
-import javax.swing.BorderFactory
 
 object Game extends SimpleSwingApplication {
 
