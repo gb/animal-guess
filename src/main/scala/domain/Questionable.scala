56e8530d248e274cb7c6365a50161eeea164e236
Gabe Tobias <gabrielsuch@gmail.com>
2024-03-29 14:12:35 -0700

    refactor: rename variable label -> answer

--- src/main/scala/domain/Questionable.scala
+++ src/main/scala/domain/Questionable.scala
@@ -2,7 +2,7 @@ package domain
 
 import adapter.UIAdapter
 
-abstract class Questionable(val label: String) {
+abstract class Questionable(val answer: String) {
 
   def question: String
   def yes(adapter: UIAdapter): Questionable
@@ -11,9 +11,9 @@ abstract class Questionable(val label: String) {
   def makeQuestion(adapter: UIAdapter) =
     if (adapter.makeQuestion(this)) yes(adapter) else no(adapter)
 
-  override def toString() = getClass().getSimpleName() + " : " + label
+  override def toString() = getClass().getSimpleName() + " : " + answer
 
   override def equals(obj: scala.Any): Boolean =
-    obj.getClass == getClass && obj.asInstanceOf[Questionable].label == label
+    obj.getClass == getClass && obj.asInstanceOf[Questionable].answer == answer
 
 }
