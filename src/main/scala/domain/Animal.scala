db89e42b3bb0749250e395c72cc707a824928ba7
Gabe Tobias <gabrielsuch@gmail.com>
2024-03-29 14:09:48 -0700

    refactor: variable rename label -> answer

--- src/main/scala/domain/Animal.scala
+++ src/main/scala/domain/Animal.scala
@@ -3,9 +3,9 @@ package domain
 import scala.Boolean._
 import adapter.UIAdapter
 
-class Animal(override val label : String) extends Questionable(label) {
+class Animal(override val answer : String) extends Questionable(answer) {
   
-  	override def question = "My guess is: " + label + "! Am I right?"
+  	override def question = "My guess is: " + answer + "! Am I right?"
 
 	def yes(adapter: UIAdapter) = {	
 		adapter.displayMessage("I knew it all along! :-)")
@@ -14,7 +14,7 @@ class Animal(override val label : String) extends Questionable(label) {
 
 	def no(adapter: UIAdapter) = {
 		val animalName = adapter.makeTextQuestion("I Give up! What animal do you have thought?")
-		val characteristic = adapter.makeTextQuestion("A(n) " + animalName + " ________ but not a(n) " + label)
+		val characteristic = adapter.makeTextQuestion("A(n) " + animalName + " ________ but not a(n) " + answer)
 		
 		new Characteristic(characteristic, new Animal(animalName), this)
 	}
